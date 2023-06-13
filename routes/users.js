9c8e7287b9f93e6c4c188e0f355275f0dfa949bb
Pradeep Kumar <pradeep.kumar@adintors.com>
2023-06-13 15:56:11 +0530

    updated

--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,17 @@ router.use(useragent.express());
 
 
 router.get('/', (req, res) => {
-  const deviceName = req.useragent.source;
-  res.send(`Your device name is: ${deviceName}`);
+  res.send(`redirect to /ip_add`);
 });
 
-
-
-// Custom middleware to get the client's IP address
-router.use((req, res, next) => {
-  const ipAddress = proxyAddr(req, 'uniquelocal');
-  req.clientIP = ipAddress;
-  next();
-});
 
 router.get('/ip_add', (req, res) => {
-  const ipAddress = req.clientIP;
-  res.send(`Your IP address is: ${ipAddress}`);
+  const ip = 
+        request.headers['cf-connecting-ip'] ||  
+        request.headers['x-real-ip'] ||
+        request.headers['x-forwarded-for'] ||
+        request.socket.remoteAddress || '';
+  res.send(`Your IP address is: ${ip}`);
 });
 
 
