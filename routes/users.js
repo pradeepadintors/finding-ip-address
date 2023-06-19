bc5fd12c37f52f252e06fb7f0ecd9fb54009df5d
Pradeep Kumar <pradeep.kumar@adintors.com>
2023-06-19 15:28:31 +0530

    private ip

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const proxyAddr = require('proxy-addr');
 const useragent = require('express-useragent');
 const geoip = require('geoip-lite');
+const dns = require('dns');
 
 
 router.use(useragent.express());
@@ -12,12 +13,35 @@ router.use(useragent.express());
 
 
 router.get('/ip_add', async(req, res) => {
-  const Data = await axios.get('https://ipapi.co/json');
-  res.send({
-    ip_address: Data.data.ip,
-    country_name: Data.data.country_name,
-    country_Code: Data.data.country_code
-  });
+  // const Data = await axios.get('https://ipapi.co/json');
+  // const ipAddress = Data.data.ip;
+  const ipAddress = req.connection.remoteAddress;
+  res.json({ ipAddress });
+    // dns.reverse(ipAddress, (err, hostnames) => {
+    //   if (err) {
+    //     console.error('Error retrieving hostname:', err);
+    //     return;
+    //   }
+    //   if (hostnames.length > 0) {
+    //     console.log("entered")
+    //     const hostname = hostnames[0];
+    //     res.send({
+    //       ip_address: Data.data.ip,
+    //       country_name: Data.data.country_name,
+    //       country_Code: Data.data.country_code,
+    //       region:Data.data.region,
+    //       deviceName: hostname
+    //     });
+    //   } else {
+    //     res.send({
+    //       ip_address: ipAddress,
+    //       country_name: Data.data.country_name,
+    //       country_Code: Data.data.country_code,
+    //       region:Data.data.region,
+    //       deviceName: "unknown"
+    //     });
+    //   }
+    // });
 });
 
 
@@ -25,32 +49,6 @@ router.get('/ip_add', async(req, res) => {
 router.get('/api/ip', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   res.json({ ip });
-});
-
-router.get('/login', (req, res) => {
-  // Extract user agent information from the request
-  const userAgent = req.useragent;
-
-  // Get browser and device information
-  const browser = userAgent.browser;
-  const device = userAgent.platform;
-
-  // Get the client's IP address
-  const ip = req.ip;
-
-  // Use the IP address to determine the location
-  const geo = geoip.lookup(ip);
-  const location = geo ? `${geo.city}, ${geo.country}` : 'Unknown';
-
-  // Store the information in a database or any other storage mechanism
-  // For simplicity, we'll just log the information to the console here
-  const lastLoginTime = new Date();
-  console.log(`Browser: ${browser} on ${device}`);
-  console.log(`Location: ${location}`);
-  console.log(`Last Login Time: ${lastLoginTime}`);
-
-  // Send a response back to the client
-  res.send('Login successful!');
 });
 
 module.exports = router;
