17409c963635540de43d54b5ff6dba6e453dce67
Pradeep Kumar <pradeep.kumar@adintors.com>
2023-06-19 15:33:25 +0530

    ip updated

--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,14 @@ router.use(useragent.express());
 router.get('/ip_add', async(req, res) => {
   // const Data = await axios.get('https://ipapi.co/json');
   // const ipAddress = Data.data.ip;
-  const ipAddress = req.connection.remoteAddress;
-  res.json({ ipAddress });
+  const ipAddress =
+    req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  // Extract the first private IP address from the list
+  const privateIp =
+    ipAddress && ipAddress.includes(',') ? ipAddress.split(',')[0] : ipAddress;
+
+  res.json({ ipAddress: privateIp });
     // dns.reverse(ipAddress, (err, hostnames) => {
     //   if (err) {
     //     console.error('Error retrieving hostname:', err);
